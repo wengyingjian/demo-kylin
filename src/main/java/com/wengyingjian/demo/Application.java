1541185d016816b8c9d6cc7b9e206ef64158e4aa
wengyingjian <q291611265@gmail.com>
2016-01-22 20:29:35 +0800

    可运行

---
+++ src/main/java/com/wengyingjian/demo/Application.java
@@ -0,0 +1,16 @@
+package com.wengyingjian.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wengyingjian on 16/1/22.
+ */
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
