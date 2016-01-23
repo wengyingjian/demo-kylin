0adcf7c07298a16f3dee6e058f403d51b55a5c20
wengyingjian <q291611265@gmail.com>
2016-01-24 00:06:15 +0800

    加入对应的dao，mapper

---
+++ demo-service/src/main/java/com/wengyingjian/demo/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.wengyingjian.demo.dao;
+
+/**
+ * Created by wengyingjian on 16/1/24.
+ */
+public class UserDao {
+}
---
+++ demo-service/src/main/java/com/wengyingjian/demo/dao/mapper/UserMapper.java
@@ -0,0 +1,7 @@
+package com.wengyingjian.demo.dao.mapper;
+
+/**
+ * Created by wengyingjian on 16/1/24.
+ */
+public interface UserMapper {
+}
