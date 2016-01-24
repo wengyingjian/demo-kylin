36d869bf43722db2b7d0e45003b2387c3c6b9a20
wengyingjian <q291611265@gmail.com>
2016-01-24 14:00:31 +0800

    kylin-rpc:demo-service添加roc-server模块

--- demo-service/src/main/java/com/wengyingjian/demo/service/UserServiceImpl.java
+++ demo-service/src/main/java/com/wengyingjian/demo/service/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.wengyingjian.demo.model.User;
 import com.wengyingjian.demo.model.query.UserQuery;
 import com.wengyingjian.kylin.common.model.Result;
 import com.wengyingjian.kylin.common.utils.ResultUtil;
+import com.wengyingjian.kylin.rpc.server.annotation.RemoteService;
+import com.wengyingjian.kylin.rpc.server.annotation.ServiceType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import java.util.List;
 /**
  * Created by wengyingjian on 16/1/24.
  */
+@RemoteService(serviceType = ServiceType.HESSIAN, serviceInterface = UserService.class, exportPath = "/userService")
 @Service
 public class UserServiceImpl implements UserService {
 
